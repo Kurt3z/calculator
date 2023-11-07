87fc98e13168a5b2413f632bb033d5a084fa3e6d
Tiago Pinto <tiago.webdev10@gmail.com>
2023-11-07 22:38:25 +0000

    feat: Display numbers and clear features

--- script.js
+++ script.js
@@ -14,23 +14,50 @@ const divide = function (a, b) {
     return a / b;
 };
 
-let firstNumber;
+const modulo = function (a, b) {
+    return a % b;
+};
+
+let firstNumber = 0;
 let operator;
-let secondNumber;
+let secondNumber = 0;
+let result;
 
 const operate = function (operator, num1, num2) {
     switch (operator) {
         case '+':
-            add(num1, num2);
-            break;
+            return add(num1, num2);
         case '-':
-            subtract(num1, num2);
-        case 'x':
-            multiply(num1, num2);
-            break;
-        case '/':
-            divide(num1, num2);
+            return subtract(num1, num2);
+        case '×':
+            return multiply(num1, num2);
+        case '÷':
+            return divide(num1, num2);
+        case '%':
+            return modulo(num1, num2);
         default:
             break;
     }
 };
+
+//////////////////////////////////////////////////////
+// DOM Elements
+const numbersBtn = document.querySelectorAll('.number');
+const operatorsBtn = document.querySelectorAll('.operator');
+const lastNumberBox = document.querySelector('#last-number');
+const currentNumberBox = document.querySelector('#current-number');
+const equalsBtn = document.querySelector('#equals');
+const clearBtn = document.querySelector('#clear');
+const deleteBtn = document.querySelector('#delete');
+
+numbersBtn.forEach(btn => {
+    btn.addEventListener('click', function (e) {
+        currentNumberBox.textContent += e.target.textContent;
+    });
+});
+
+clearBtn.addEventListener('click', function () {
+    firstNumber, secondNumber, (result = 0);
+    currentNumberBox.textContent = '';
+    lastNumberBox.textContent = '';
+});
